d0ec1c7eddcb671165257a614dc7885bf8fef183
Dori <jaewan0114@naver.com>
2022-10-07 04:30:49 +0900

    POPULAR_GAME_SLIDE

---
+++ frontend/src/elements/Popular_game_slide.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const popular_game_slide = (props) => {
+
+    return (
+
+        <div className="ad_game_font">
+                9월 넷째주 게임
+        </div>
+
+
+    );
+};
+
+export default popular_game_slide;
