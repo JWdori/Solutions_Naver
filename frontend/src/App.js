80f5a1637dc7383cd2f5b6cd90e1c8a851c7db5f
Dori <jaewan0114@naver.com>
2022-10-08 06:13:10 +0900

    modal popup issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import Header from "./elements/Header";
 import Loading from "./pages/Loading";
 import Test from "./pages/Test"
+import recgogo from "./elements/Popup_rec_gogo";
+
 
 function App() {
     return (
@@ -13,7 +15,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route element={<Home/>} path={"/"}/>
-                    <Route element={<Loading/>} path={"/loading"}/>
+                    <Route element={<recgogo/>} path={"/recgogo"}/>
                     <Route element={<Test/>} path={"/test"}/>
                 </Routes>
             </BrowserRouter>
