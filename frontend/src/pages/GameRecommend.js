ec57e4664095034f2f86f36bc32d964a99e29d0b
222yujin <222yujin@naver.com>
2022-10-20 23:54:13 +0900

    Editing GameRecommend

--- frontend/src/pages/GameRecommend.js
+++ frontend/src/pages/GameRecommend.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../elements/Header";
 import ProgressRec from "../elements/Progress_rec";
-
+import type1 from "../images/type1.png"
 
 const GameRecommend = (props) => {
 
@@ -9,7 +9,23 @@ const GameRecommend = (props) => {
     return (
         <>
             <Header isBack={false}/>
-            <ProgressRec width={300} step={step}/>
+            <ProgressRec width={"300px"} step={step}/>
+
+            <div className={"recGame"}>
+                <img
+                    className="recGameImg"
+                    src={type1}
+                    alt="img"
+                    width={"300px"}
+                    height={"165px"}
+                ></img>
+                <div className={"recGameDesc"}>
+                    - 한국밈 피하기 -
+                    <span>
+                        정시넝ㅄ이 나오는 패턴을 피해서 최고점수에 도달하라!
+                    </span>
+                </div>
+            </div>
         </>
     );
 };
