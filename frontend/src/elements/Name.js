9a9e724ccd17dae3b003953bd746292a8f14b8c6
Dori <jaewan0114@naver.com>
2022-11-08 02:14:52 +0900

    refactoring_design

--- frontend/src/elements/Name.js
+++ frontend/src/elements/Name.js
@@ -9,20 +9,8 @@ const Name = ({nameFunction}) => {
     }
     return (
         <>
-            <div className="serviceTitleBox" style={{marginTop: "50px"}}>
-                <div className="NameQ" style={{color: "white"}}>
-                    당신의&nbsp;
-                </div>
-                <div className="NameQ">
-                    이름
-                </div>
-                <div className="NameQ" style={{color: "white"}}>
-                    은 . . . ?
-                </div>
 
-            </div>
-
-            <input type="text" className="NameBox" maxLength='6' style={{textAlign: "center", borderWidth: "2px"}}
+            <input type="text" className="NameBox" name="dd"maxLength='6' placeholder="닉네임을 입력하세요" style={{color:"#FFAE68", marginTop:"80px"}}
                    onChange={onChangeName}>
             </input>
 
